b7d318a78fc19ac81c6a542cdae4f13869c678dd
josedantearroyo <malekframi@gmail.com>
2025-09-21 08:52:26 -0500

    header primera version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,13 @@ export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
+  darkMode:'class',
   theme: {
-    extend: {},
+    extend: {
+        fontFamily:{
+            sans:['Inter', 'ui-sans-serif']
+        }
+    },
   },
   plugins: [],
 }
